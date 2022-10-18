2534657f3515190defa262b44dc0bc08486f02f5
Osundaro <osuntemiloluwa@gmail.com>
2022-10-18 11:31:11 -0700

    advanced task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,46 @@
-include "main.h"
-
+#include "main.h"
 /**
- * print_times_table - Prints the n times table
- *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
- */
+* print_times_table - prints the times table for n.
+* @n: The multiplication table requested.
+* Return: Nothing.
+*/
 void print_times_table(int n)
 {
-	int a, b, op;
-
-	if (n >= 0 && n <= 15)
-	{
-		for (a = 0; a <= n; a++)
-		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
-		}
-	}
+int i, j, res;
+if (!(n > 15 || n < 0))
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+res = (i * j);
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (res < 10 && j != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else if (res >= 10 && res < 100)
+{
+_putchar(' ');
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res >= 100 && j != 0)
+{
+_putchar((res / 100) + '0');
+_putchar((res / 10) % 10 + '0');
+_putchar((res % 10) + '0');
+}
+else
+_putchar((res % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
